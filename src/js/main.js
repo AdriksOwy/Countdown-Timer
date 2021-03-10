dc41bba6450ee91ba0d0310446e717516872fd9d
Adriks <lepekadrian123@gmail.com>
2021-03-10 22:30:40 +0100

    feat: getElementById

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
-document.write("");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
