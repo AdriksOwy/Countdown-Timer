0c17c8ad5dc0e004a90859a2fc261dfb4c32bcfe
Adriks <lepekadrian123@gmail.com>
2021-03-11 10:28:23 +0100

    feat: new variable

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,5 @@ const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
+
+const newYear = "30 Jul 2021";
