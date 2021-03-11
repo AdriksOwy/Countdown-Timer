440db61ad7dab1d7123e38faf998347eb8562200
Adriks <lepekadrian123@gmail.com>
2021-03-11 11:41:19 +0100

    fix: countdown timer live

--- src/js/main.js
+++ src/js/main.js
@@ -6,24 +6,26 @@ const secondsElement = document.getElementById("seconds");
 const newYear = "30 Jul 2021";
 
 function countdown() {
-  const newYearDate = new Date(newYear);
-  const currentDate = new Date();
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
 
-  const totalSeconds = (newYearDate - currentDate) / 1000;
+    const totalSeconds = (newYearDate - currentDate) / 1000;
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const mins = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds) % 60;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-  daysElement.innerHTML = days;
-  hoursElement.innerHTML = TimeChange(hours);
-  minsElement.innerHTML = TimeChange(mins);
-  secondsElement.innerHTML = TimeChange(seconds);
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = TimeChange(hours);
+    minsElement.innerHTML = TimeChange(mins);
+    secondsElement.innerHTML = TimeChange(seconds);
 }
 
 countdown();
 
 function TimeChange(time) {
-  return time < 10 ? `0${time}` : time;
+    return time < 10 ? `0${time}` : time;
 }
+
+setInterval(countdown, 1000);
