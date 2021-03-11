e1efeda7cbbbcde7e25a489c459cc5aae1599d64
Adriks <lepekadrian123@gmail.com>
2021-03-11 10:39:32 +0100

    fix: calculation hours

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ function countdown() {
   const totalSeconds = (newYearDate - currentDate) / 1000;
 
   const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
   const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 }
