bdf8ee4fc17a4248975a05c49dbde556e7bfea26
Adriks <lepekadrian123@gmail.com>
2021-03-11 10:34:41 +0100

    feat: countdown function

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,10 @@ const mins = document.getElementById("mins");
 const seconds = document.getElementById("seconds");
 
 const newYear = "30 Jul 2021";
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+}
